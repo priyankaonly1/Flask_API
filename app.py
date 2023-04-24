b0be6bb73976971dcbd33d11c583a5b3b0f859a5
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2023-04-25 01:30:58 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import secrets
+from flask_jwt_extended import JWTManager
+
+from flask import Flask
+from flask_smorest import Api
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
+from db import db
+import models
+
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
+    jwt = JWTManager(app)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
+
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
+    
+    return app
+
+# if __name__ == "__main__":
+#     app.run()
